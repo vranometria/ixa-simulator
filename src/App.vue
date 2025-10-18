d1fc352896fa4f5ca6e7ff67d35eaeb89da0900f
vranometria <hizakiri@gmail.com>
2025-10-18 19:59:55 +0900

    登録画面を別タブに分割

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,65 @@
 <template>
-    <BusyoEditor />
+    <div class="tabs">
+        <div :class="'tab ' + selected('0')" @click="onClicked" :value="0">
+            武将登録/編集
+        </div>
+        <div :class="'tab ' + selected('1')" @click="onClicked" :value="1">
+            本丸防御陣形
+        </div>
+    </div>
 
-    <FormationEditor />
+    <BusyoEditor v-if="page === '0'" />
+
+    <FormationEditor v-if="page === '1'" />
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import BusyoEditor from './BusyoEditor.vue';
 import FormationEditor from './FormationEditor.vue';
 import { useBusyoStore } from './store/busyoStore';
 const busyoStore = useBusyoStore();
+const page = ref("0");
+
+function onClicked(e: PointerEvent) {
+    const div:HTMLDivElement = e.currentTarget as HTMLDivElement;
+    page.value = div.getAttribute("value");
+}
+
+const selected = (val:string) => {
+    return (page.value === val) ? "true" : "false";
+};
+
 </script>
+
+
+<style scoped>
+.tabs {
+    display: flex;
+    margin-bottom: 16px;
+}
+
+.true {
+    background-color: lightgray;
+}
+
+.tab {
+    padding: 8px 16px;
+    cursor: pointer;
+    margin-right: 8px;
+}
+
+.tab:hover {
+    background-color: #e0e0e0;
+}
+
+.tab h2 {
+    margin: 0;
+    font-size: 16px;
+}
+.tab[selected] {
+    background-color: #fff;
+    border-bottom: 2px solid #fff;
+    font-weight: bold;
+}
+</style>
